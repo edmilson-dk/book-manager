ffb44c3c283502ea88ff7c99a785f1de951a635c
edmilson-dk <droidvidaboa@gmail.com>
2021-07-12 10:42:17 -0300

    feat: created dashboard home page with initial html structure

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,9 @@
+import { DashboardLayout } from "src/components/dashboard-components/dashboard-layout";
+
+export default function Dashboard() {
+  return (
+    <DashboardLayout>
+      <h1>dashboard</h1>
+    </DashboardLayout>
+  );
+}
