985ca07b281879b5e2380f47f6c49cf60362730c
edmilson-dk <droidvidaboa@gmail.com>
2021-07-14 21:04:10 -0300

    feat: added recents books section

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,56 @@
+import Image from "next/image";
 import { DashboardLayout } from "src/components/dashboard-components/dashboard-layout";
+import { DashboardSearchInput } from "src/components/dashboard-components/dashboard-search-input";
+import { TitleComponent } from "src/components/dashboard-components/titles";
+
+import styles from "src/styles/pages/dashboard/styles.module.scss";
 
 export default function Dashboard() {
   return (
     <DashboardLayout>
-      <h1>dashboard</h1>
+      <section className={styles.dashboardHomeWrapper}>
+        <article className={styles.dashboardHomeContent}>
+          <DashboardSearchInput />
+          <div>
+            <TitleComponent text="Recents" />
+
+            <article>
+              <Image
+                src="/images/recents/regex-book.png"
+                alt="Regex Book"
+                width={112}
+                height={160}
+                objectFit="cover"
+              />
+              <Image
+                src="/images/recents/ref-ebook.png"
+                alt="Regex Book"
+                width={112}
+                height={160}
+                objectFit="cover"
+              />
+              <Image
+                src="/images/recents/ddd-ebook.png"
+                alt="Regex Book"
+                width={112}
+                height={160}
+                objectFit="cover"
+              />
+              <Image
+                src="/images/recents/gof-ebook.png"
+                alt="Regex Book"
+                width={112}
+                height={160}
+                objectFit="cover"
+              />
+            </article>
+          </div>
+
+        </article>
+        <aside className={styles.dashboardHomeSidebar}>
+          <h2>Dashboard</h2>
+        </aside>
+      </section>
     </DashboardLayout>
   );
 }
