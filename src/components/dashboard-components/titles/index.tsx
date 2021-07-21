1de3f4a00b9c9aec91960cdf1a21c72d928c940b
edmilson-dk <droidvidaboa@gmail.com>
2021-07-21 08:36:45 -0300

    feat: added title primary color property

--- src/components/dashboard-components/titles/index.tsx
+++ src/components/dashboard-components/titles/index.tsx
@@ -2,9 +2,9 @@ import { TitleProps } from "./types";
 
 import styles from "src/styles/components/dashboard-components/titles/styles.module.scss";
 
-export function TitleComponent({ text }: TitleProps) {
+export function TitleComponent({ text, color }: TitleProps) {
   return (
-    <h2 className={styles.titlePrimary}>
+    <h2 className={styles.titlePrimary} style={{ color }}>
       {text}
     </h2>
   );
